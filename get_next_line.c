5d381021d32c3af749ef6dbc94a3d73e000f9cd0
Hugo Taillefumier <htaillef@z1r10p4.le-101.fr>
2017-11-30 15:38:39 +0100

    functionnal unormed

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: htaillef <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/29 17:04:25 by htaillef     #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/29 18:56:55 by htaillef    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/11/30 15:37:15 by htaillef    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -30,16 +30,16 @@ int		index_of(const char *s, int c)
 
 char	*process(char **stock, int *error)
 {
-	int	pos;
+	int		pos;
 	char	*res;
 	char	*stock_ret;
 
-	if ((pos = index_of(*stock, '\n')) >= 0) // \n found
+	if ((pos = index_of(*stock, '\n')) >= 0)
 	{
 		stock_ret = *stock;
-		if (!(res = ft_strsub(*stock, 0, pos)) || // get result line
-		    !(*stock = ft_strsub(*stock, pos + 1, // update stock
-		    ft_strlen(*stock) - pos + 1)))
+		if (!(res = ft_strsub(*stock, 0, pos)) ||
+				!(*stock = ft_strsub(*stock, pos + 1,
+						ft_strlen(*stock) - pos + 1)))
 		{
 			*error = 1;
 			return (NULL);
@@ -51,18 +51,17 @@ char	*process(char **stock, int *error)
 	return (NULL);
 }
 
-int	get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	int			ret;
-	int			error;
+	int				ret;
+	int				error;
 	char			buffer[BUFF_SIZE + 1];
 	static char		*stock;
 	char			*stock_ret;
 	char			*res;
 
-	if (fd == -1)
+	if (fd < 0 || !(line))
 		return (-1);
-
 	error = 0;
 	if (stock)
 	{
@@ -74,28 +73,33 @@ int	get_next_line(const int fd, char **line)
 		else if (error)
 			return (-1);
 	}
-
 	if (!stock)
 		if (!(stock = ft_strdup("")))
-			return (-1);				// strdup failed
-
+			return (-1);
 	while ((ret = read(fd, buffer, BUFF_SIZE)))
 	{
 		buffer[ret] = '\0';
 		stock_ret = stock;
-		if (!(stock = ft_strjoin(stock, buffer)))
-			return (-1);				// strjoin failed
+		if (ret == -1 || !(stock = ft_strjoin(stock, buffer)))
+			return (-1);
 		free(stock_ret);
 		stock_ret = NULL;
 		error = 0;
 		if ((res = process(&stock, &error)))
 		{
 			*line = res;
-			return (1);				// process succes
+			return (1);
 		}
 		else if (error)
-			return (-1);				// process failed
+			return (-1);
 	}
-	
+	if (!res && ft_strlen(stock) > 0)
+	{
+		if (!(*line = ft_strdup(stock)))
+			return (-1);
+		free(stock);
+		stock = NULL;
+		return (1);
+	}
 	return (0);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: htaillef <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2017/11/29 17:48:44 by htaillef     #+#   ##    ##    #+#       */
-/*   Updated: 2017/11/29 18:51:07 by htaillef    ###    #+. /#+    ###.fr     */
+/*   Updated: 2017/11/30 14:58:59 by htaillef    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,9 +14,10 @@
 #include "get_next_line.h"
 #include <fcntl.h>
 #include <stdio.h>
+
 int		main(int argc, char **argv)
 {
-	int	fd;
+	int		fd;
 	char	*line;
 
 	if (argc != 2)
@@ -24,7 +25,7 @@ int		main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 		return (0);
-	while (get_next_line(0, &line))
+	while (get_next_line(fd, &line))
 		printf("%s\n", line);
 	close(fd);
 	return (0);
